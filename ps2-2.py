ce3b9125402bb01d3b5a7ba4f7f5c9e4b203f690
dixoncox <dixonbush@yahoo.com>
2014-03-04 14:13:37 -0800

    finally works

--- ps2-2.py
+++ ps2-2.py
@@ -3,6 +3,8 @@ annualInterestRate = float(raw_input('Enter annual interest rate:'))
 monthlyPaymentRate = float(raw_input('Enter monthly payment rate:'))
 monthlyInterestRate = annualInterestRate/12.0
 updatedMonthlyBalance = balance
+totalPaid = 0
+print
 for i in range (1,13):
     #balance = balance + balance*annualInterestRate/12.0
     #minPayment = (balance + balance*annualInterestRate/12.0)*monthlyPaymentRate
@@ -10,6 +12,9 @@ for i in range (1,13):
     minPayment = updatedMonthlyBalance * monthlyPaymentRate
     monthlyUnpaidBalance = updatedMonthlyBalance - minPayment
     updatedMonthlyBalance = monthlyUnpaidBalance + monthlyInterestRate*monthlyUnpaidBalance
+    totalPaid = totalPaid + minPayment
     print 'Month:',i
     print 'Minimum monthly payment:', round(minPayment,2)
     print 'Remaining balance:', round(updatedMonthlyBalance,2)
+print 'Total paid:', round(totalPaid,2)
+print 'Remaining balance:', round(updatedMonthlyBalance,2)
