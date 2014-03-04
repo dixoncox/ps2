9df83daced4f5319650e8b13b9d98df6a7b80737
dixoncox <dixonbush@yahoo.com>
2014-03-04 01:13:43 -0800

    last commit for Monday night

--- ps2.py
+++ ps2.py
@@ -1,3 +1,11 @@
-balance = raw_input('Enter starting balance:')
-annualInterestRate = raw_input('Enter annual interest rate:')
-monthlyPaymentRate = raw_input('Enter monthly payment rate:')
+balance = float(raw_input('Enter starting balance:'))
+annualInterestRate = float(raw_input('Enter annual interest rate:'))
+monthlyPaymentRate = float(raw_input('Enter monthly payment rate:'))
+for i in range (1,13):
+    minPayment = balance*(1+(annualInterestRate/12.0))*monthlyPaymentRate
+    balance = balance - minPayment
+    print 'Month:',i
+    print 'Minimum monthly payment:', minPayment
+    print 'Remaining balance:', balance
+    
+    
